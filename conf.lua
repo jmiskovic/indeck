4fd5192172153f47e54d449ce8bdaf231a459d8e
Josip Miskovic <josipmiskovic@gmail.com>
2022-11-26 18:13:57 +0100

    Adapt conf to be versatile

--- conf.lua
+++ conf.lua
@@ -2,6 +2,7 @@ lovr.conf = function(t)
   t.title, t.identity = "inDeck", "indeck"
   t.saveprecedence = false
   t.window.resizable = true
-  t.window.width = 1200
-  t.window.height = 1600
+  t.graphics.shadercache = false
+  t.graphics.stencil = true
+  t.headset.stencil = true
 end
